9c63c674fdb37878ddd13b8012c11d8c81750a66
Nick Young <ynick@vmware.com>
2019-11-29 11:03:52 +1100

    Update with initial test of HTTPProxy conversion
    Signed-off-by: Nick Young <ynick@vmware.com>

--- cmd/ir2proxy/main.go
+++ cmd/ir2proxy/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"io/ioutil"
 	"os"
 
+	"github.com/davecgh/go-spew/spew"
 	irv1beta1 "github.com/projectcontour/contour/apis/contour/v1beta1"
 	contourscheme "github.com/projectcontour/contour/apis/generated/clientset/versioned/scheme"
+	hpv1 "github.com/projectcontour/contour/apis/projectcontour/v1"
 	"github.com/sirupsen/logrus"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/scheme"
 )
 
@@ -35,18 +39,31 @@ func main() {
 	}
 
 	contourscheme.AddToScheme(scheme.Scheme)
-	// This doesn't currently work with multiple objects in the one file.
-	decode := scheme.Codecs.UniversalDeserializer().Decode
 
-	ir, groupVersionKind, err := decode(data, nil, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	switch t := ir.(type) {
-	case *irv1beta1.IngressRoute:
-		log.Infof("IngressRoute %s, namespace %s", t.ObjectMeta.Name, t.ObjectMeta.Namespace)
-	default:
-		log.Infof("This utility only works with IngressRoute, a %s was supplied.", groupVersionKind)
+	for _, yamldoc := range bytes.Split(data, []byte("---")) {
+		if len(yamldoc) == 0 {
+			continue
+		}
+
+		decode := scheme.Codecs.UniversalDeserializer().Decode
+
+		ir, groupVersionKind, err := decode(yamldoc, nil, nil)
+		if err != nil {
+			log.Infof("Skipped yaml doc, %s", err)
+			continue
+		}
+		switch t := ir.(type) {
+		case *irv1beta1.IngressRoute:
+			log.Infof("IngressRoute %s, namespace %s, labels %s", t.ObjectMeta.Name, t.ObjectMeta.Namespace, t.ObjectMeta.Labels)
+			hp, err := translateIngressRouteToHTTPProxy(t)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Error(spew.Sdump(hp))
+		default:
+			log.Errorf("This utility only works with IngressRoute, a %s was supplied.", groupVersionKind)
+		}
+
 	}
 
 }
@@ -58,4 +75,22 @@ func verifyYAMLFile(filename string) bool {
 	}
 	return !info.IsDir()
 
+}
+
+func translateIngressRouteToHTTPProxy(ir *irv1beta1.IngressRoute) (hp *hpv1.HTTPProxy, err error) {
+
+	hp = &hpv1.HTTPProxy{
+		ObjectMeta: v1.ObjectMeta{
+			Name:        ir.ObjectMeta.Name,
+			Namespace:   ir.ObjectMeta.Namespace,
+			Labels:      ir.ObjectMeta.DeepCopy().GetLabels(),
+			Annotations: ir.ObjectMeta.DeepCopy().GetAnnotations(),
+		},
+	}
+
+	if ir.Spec.VirtualHost != nil {
+		hp.Spec.VirtualHost = ir.Spec.VirtualHost
+	}
+
+	return hp, err
 }
