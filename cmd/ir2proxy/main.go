dbef126f82c4e960672ecd2b30dcdecf6ae9039e
Nick Young <ynick@vmware.com>
2019-12-09 17:14:49 +1100

    Remove createdTimestamp field from all output
    Fixes #8.
    
    Signed-off-by: Nick Young <ynick@vmware.com>

--- cmd/ir2proxy/main.go
+++ cmd/ir2proxy/main.go
@@ -66,6 +66,10 @@ func run() int {
 			log.Warn(err)
 			return 1
 		}
+		// The Kubernetes standard header field `currentTimestamp` serializes weirdly,
+		// so filter it out.
+		// See https://github.com/projectcontour/ir2proxy/issues/8 for more explanation here.
+		outputYAML = bytes.ReplaceAll(outputYAML, []byte("  creationTimestamp: null\n"), []byte(""))
 		fmt.Printf("---\n%s", outputYAML)
 	}
 
